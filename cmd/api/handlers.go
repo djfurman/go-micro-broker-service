40d233bf20a6e7d190410d97ed47abb978838638
Daniel Furman <djfurman@gmail.com>
2022-12-11 17:57:44 -0500

    Add broker routes for the broker.

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -12,6 +12,7 @@ type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Log    LogPayload  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
 }
 
 type AuthPayload struct {
@@ -22,6 +23,13 @@ type AuthPayload struct {
 type LogPayload struct {
 	Name string `json:"name"`
 	Data string `json:"data"`
+}
+
+type MailPayload struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -47,6 +55,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, requestPayload.Auth)
 	case "log":
 		app.logItem(w, requestPayload.Log)
+	case "mail":
+		app.sendMail(w, requestPayload.Mail)
 	default:
 		app.errorJSON(w, errors.New("unknown action"), http.StatusBadGateway)
 	}
@@ -137,6 +147,44 @@ func (app *Config) logItem(w http.ResponseWriter, i LogPayload) {
 	var payload jsonResponse
 	payload.Error = false
 	payload.Message = "logged"
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) sendMail(w http.ResponseWriter, mailMessage MailPayload) {
+	jsonData, err := json.Marshal(mailMessage)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	mailServiceURL := "http://mail-service/send"
+
+	request, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err, http.StatusFailedDependency)
+		return
+	}
+
+	request.Header.Set("content-type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusFailedDependency)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("Error calling mail service"), http.StatusFailedDependency)
+		log.Println(response)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "mail message sent"
 
 	app.writeJSON(w, http.StatusAccepted, payload)
 }
